4de4d2242c5fe59522ce796f69213be352492dea
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-05-23 08:20:48 +0000

    pee

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,33 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import React, { useState } from "react";
+import "../css/custom.css";
+import Layout from "@theme/Layout";
+// import styles from "./team.module.css";
+
+import clsx from "clsx";
 
 export default function Home() {
-  return <div className={styles.container}>Pwetty Pwease</div>;
+  return (
+    <Layout
+      title="Our Thyck Team"
+      description="Meet the people behind Thyck Corgis."
+    >
+      <section className={styles.person}>
+        <div className="container">
+          <p className="title text--center slide-in-left">
+            Meet the people behind Thyck Corgis.
+          </p>
+          <div
+            className="headerLine centered slide-in-left"
+            style={{ animationDelay: "0.5s" }}
+          />
+          <div style={{ height: "69px" }} />
+          <div className="row">
+            {TeamList.map((props, idx) => (
+              <Team key={idx} {...props} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </Layout>
+  );
 }
